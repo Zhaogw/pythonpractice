a3edb756ca478dd16d4c12b2b4849ff13af205df
xiaosha <574776424@qq.com>
2020-05-06 12:53:12 +0800

    提交九宫格、六宫格切图、画线、分部位切图py文件

---
+++ faceTrain/nineGrid.py
@@ -0,0 +1,58 @@
+# 导入相关的库
+from PIL import Image
+import os
+import time
+
+files_list =[]
+for files in os.walk("E:\\记忆\\脸谱\\选手记忆93张\\"):
+    files_list.append(files)
+files_list=files_list[0][2]
+
+
+
+input_route = 'E:\\记忆\\脸谱\\选手记忆93张\\'
+output_route = "E:\\记忆\\脸谱\\选手记忆93张切割图片\\"
+# output_route1 = "E:\\记忆\\脸谱\\lianpupart1\\"
+# output_route2 = "E:\\记忆\\脸谱\\lianpupart2\\"
+# output_route3 = "E:\\记忆\\脸谱\\lianpupart3\\"
+# output_route4 = "E:\\记忆\\脸谱\\lianpupart4\\"
+# output_route5 = "E:\\记忆\\脸谱\\lianpupart5\\"
+# output_route6 = "E:\\记忆\\脸谱\\lianpupart6\\"
+# output_route7 = "E:\\记忆\\脸谱\\lianpupart7\\"
+# output_route8 = "E:\\记忆\\脸谱\\lianpupart8\\"
+# output_route9 = "E:\\记忆\\脸谱\\lianpupart9\\"
+for i in range(0, len(files_list)):
+    img = Image.open(input_route+files_list[i])
+    w = img.width/3
+    h = img.height/3
+    file_name = files_list[i][:-4]
+
+    region1 = img.crop((0, 0, w, h))
+    region1.save(output_route+file_name+'_1.png')
+
+    region2 = img.crop((w, 0, 2*w, h))
+    region2.save(output_route+file_name+'_2.png')
+
+    region3 = img.crop((2*w, 0, 3*w, h))
+    region3.save(output_route+file_name+'_3.png')
+
+    region4 = img.crop((0, h, w, 2*h))
+    region4.save(output_route+file_name+'_4.png')
+
+    region5 = img.crop((w, h, 2*w, 2*h))
+    region5.save(output_route+file_name+'_5.png')
+
+    region6 = img.crop((2*w, h, 3*w, 2*h))
+    region6.save(output_route+file_name+'_6.png')
+
+    region7 = img.crop((0, 2*h, w, 3*h))
+    region7.save(output_route+file_name+'_7.png')
+
+    region8 = img.crop((w, 2*h, 2*w, 3*h))
+    region8.save(output_route+file_name+'_8.png')
+
+    region9 = img.crop((2*w, 2*h, 3*w, 3*h))
+    region9.save(output_route+file_name+'_9.png')
+
+    print(file_name+"切割完成")
+    time.sleep(1)
---
+++ faceTrain/nineGridLine.py
@@ -0,0 +1,30 @@
+# 导入相关的库
+from PIL import Image
+from PIL import ImageDraw
+import os
+import time
+
+files_list = []
+for files in os.walk("E:\\记忆\\脸谱\\选手记忆93张\\"):
+    files_list.append(files)
+files_list = files_list[0][2]
+
+input_route = 'E:\\记忆\\脸谱\\选手记忆93张\\'
+output_route = "E:\\记忆\\脸谱\\选手记忆93张画线\\"
+for i in range(0, len(files_list)):
+    img = Image.open(input_route + files_list[i])
+    img_d = ImageDraw.Draw(img)
+    x_len = img.width
+    y_len = img.height
+    file_name = files_list[i][:-4]
+    for x in range(0, x_len, int(x_len / 3)):
+        img_d.line(((x, 0), (x, y_len)), (0, 0, 0))
+        img_d.line(((x + 1, 0), (x + 1, y_len)), (0, 0, 0))
+        img_d.line(((x + 2, 0), (x + 2, y_len)), (0, 0, 0))
+    for y in range(0, y_len, int(y_len / 3)):
+        img_d.line(((0, y), (x_len, y)), (0, 0, 0))
+        img_d.line(((0, y + 1), (x_len, y + 1)), (0, 0, 0))
+        img_d.line(((0, y + 2), (x_len, y + 2)), (0, 0, 0))
+    img.save(output_route + file_name + ".png")
+    print(file_name+'画线完成')
+    time.sleep(2)
